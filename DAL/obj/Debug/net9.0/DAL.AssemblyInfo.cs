1c1152ee91533b180b963f2dd94840f287eab1ac
DanielGus <danielgus@outlook.es>
2026-04-20 20:37:07 -0300

    4Capas

--- DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
@@ -11,15 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae12becf80f718eecb83791571e7a43a9aac1e4")]
-[assembly: System.Reflection.AssemblyProductAttribute("UI")]
-[assembly: System.Reflection.AssemblyTitleAttribute("UI")]
+[assembly: System.Reflection.AssemblyProductAttribute("DAL")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
