8356eee46e9ae421791ebdb650c350891cb33cca
Gustavo Daniel Britez Alvarenga <danielgus@outlook.es>
2026-05-19 11:52:12 -0300

    Observer_Console_M

--- Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1188faa8af4879ab1fced1b734a344b91f7bb20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+223751f774f81c77435e2ecf57d2a861d81fa141")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
