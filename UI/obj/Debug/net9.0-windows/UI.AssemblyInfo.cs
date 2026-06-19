b59313933449b43612ff3e3576d5fec225082555
Fernando <fernandopalavecino62@gmail.com>
2026-06-19 15:16:56 -0300

    CC

--- UI/obj/Debug/net9.0-windows/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net9.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063ed836e6b40dbbb45604939f28da486f094327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466afeffe0d08f66a889663ef8fa5882acb13a56")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
